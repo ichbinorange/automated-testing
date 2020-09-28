e6b4de1c4dd151993241024efc385ec6e11113e0
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-09-27 23:08:35 -0700

    pass wave1

--- lib/card.rb
+++ lib/card.rb
@@ -1,18 +1,18 @@
-
-# card.rb
+require_relative 'deck'
 
 class Card
   attr_reader :value, :suit
 
   def initialize(value, suit)
-    @value = value
-    @suit = suit
-    
+    raise ArgumentError.new("Invalid card number : #{ value }") unless (1..13).include? (value.to_i)
+    @value = value.to_i
 
+    raise ArgumentError.new("Invalid suit : #{ suit }") unless [:spades , :hearts, :diamonds, :clubs].include? (suit.downcase)  
+    @suit = suit.downcase
   end
 
   def to_s
-    return "#{value} of #{suit.to_s}"
+    return "#{ @value } of #{ @suit }"
   end
 
 end
