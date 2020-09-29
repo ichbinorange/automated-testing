82e261c48fae839d2c63164044b3f698f2395c45
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-09-29 13:31:12 -0700

    use begin in test for deck

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,17 +5,20 @@ describe Deck do
   # What should you test?
     # 1.  Expected Behavior
     # 2.  Edge cases
+  before do
+    @new_deck = Deck.new
+  end
+  
   describe "You can create a Deck instance" do
     it "Can be created with 52 cards" do
-      new_deck = Deck.new
-      expect(new_deck).must_be_instance_of Deck
+      expect(@new_deck).must_be_instance_of Deck
       
-      new_deck.cards.each do |card|
+      @new_deck.cards.each do |card|
         expect(card).must_be_instance_of Card
       end
   
-      expect(new_deck.cards).must_be_instance_of Array
-      expect(new_deck.count()).must_equal 52
+      expect(@new_deck.cards).must_be_instance_of Array
+      expect(@new_deck.count()).must_equal 52
 
       expect(Deck.new(deck_num: 2).count).must_equal 104
       
@@ -34,29 +37,26 @@ describe Deck do
   
   describe "Testing shuffle" do
     it "responds to shuffle" do
-      new_deck = Deck.new
-      expect(new_deck).must_respond_to :shuffle
+      expect(@new_deck).must_respond_to :shuffle
     end
   end  
 
   describe "Testing draw" do
     it "draw returns a readable String value" do
-      new_deck = Deck.new
-      expect(new_deck.draw).must_be_instance_of Array
+      expect(@new_deck.draw).must_be_instance_of Array
 
-      new_deck.draw.each do |card|
+      @new_deck.draw.each do |card|
         expect(card).must_be_instance_of Card
       end
     end
 
     it "draw removes cards from the Decks" do
-      new_deck = Deck.new
-      new_deck.draw
-      expect(new_deck.count()).must_equal 51
+      @new_deck.draw
+      expect(@new_deck.count()).must_equal 51
       
-      new_deck = Deck.new
-      new_deck.draw(card_num: "3")
-      expect(new_deck.count()).must_equal 49
+      draw_three_cards = Deck.new
+      draw_three_cards.draw(card_num: "3")
+      expect(draw_three_cards.count()).must_equal 49
 
       two_decks = Deck.new(deck_num: 2)
       two_decks.draw(card_num: 4)
@@ -64,17 +64,13 @@ describe Deck do
     end
 
     it "Raises an ArgumentError for invalid cards to draw" do
-      new_deck = Deck.new
-      expect{new_deck.draw(card_num: 53)}.must_raise ArgumentError
+      expect{@new_deck.draw(card_num: 53)}.must_raise ArgumentError
 
-      new_deck = Deck.new
-      expect{new_deck.draw(card_num: -1)}.must_raise ArgumentError
+      expect{@new_deck.draw(card_num: -1)}.must_raise ArgumentError
 
-      new_deck = Deck.new
-      expect{new_deck.draw(card_num: nil)}.must_raise ArgumentError
+      expect{@new_deck.draw(card_num: nil)}.must_raise ArgumentError
 
-      new_deck = Deck.new
-      expect{new_deck.draw(card_num: "test")}.must_raise ArgumentError
+      expect{@new_deck.draw(card_num: "test")}.must_raise ArgumentError
     end
   end
 
