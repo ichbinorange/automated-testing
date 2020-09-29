053cc01b7aec356fb4e8dfcd5315f425809913c8
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-09-28 19:17:56 -0700

    wave 3: add methods to Deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,19 +1,31 @@
-
-# deck.rb
-
-require_relative 'card'
+# require_relative 'card' (it would raise a warning in minitst)
 
 class Deck
+  attr_reader :cards
 
-  def initialize
-
+  def initialize(deck_num: 1)
+    @cards = Array.new
+    [:hearts, :spades, :clubs, :diamonds].each do |suit|
+      (1..13).each do |value|
+        @cards.push(Card.new(value, suit))
+      end
+    end
+    @cards *= deck_num
   end
-
-  def draw
-    # returns a card
-  end
-
+  
   def shuffle
-    # shuffles the deck
+    return @cards.shuffle
+  end
+
+  def draw(card_num: 1)
+    shuffle_card = shuffle
+    @draw = shuffle_card.first(card_num)
+    shuffle_card.shift(card_num)
+    @cards = shuffle_card
+    return @draw
+  end
+
+  def count
+    return @cards.count
   end
 end
